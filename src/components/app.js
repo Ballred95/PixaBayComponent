e349408209c6c84a9a95e246ee92d7c389c868e6
Ballred95 <blake.allred95@icloud.com>
2020-07-14 15:44:39 -0600

    search function working

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,7 @@ import Imagesearch from './image_search/imagesearch';
 
 const API_KEY = '17478784-735f4f1d92940cc091944cc00'
 
-const url = `https://pixabay.com/api/?key=${API_KEY}&q=yellow+flowers&image_type=photo`
+
 
 export default class App extends Component {
 
@@ -11,7 +11,10 @@ export default class App extends Component {
     images: []
   }
 
-  handleGetRequest = async () => {
+  handleGetRequest = async (e) => {
+    e.preventDefault()
+    const searchTerm = e.target.elements.searchValue.value
+    const url = `https://pixabay.com/api/?key=${API_KEY}&q=${searchTerm}&image_type=photo`
     const request = await fetch(url)
     const response = await request.json()
     this.setState({images: response.hits})
